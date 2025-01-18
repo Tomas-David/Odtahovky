b9c97ecab4401911b733a14e9ec857e31810d635
Tomas-David <ttomikdavid@gmail.com>
2025-01-18 19:23:44 +0100

    EF

---
+++ Odtahovky.Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Odtahovky.Server.Models;
+namespace Odtahovky.Server.Data
+{
+    public class ApplicationDbContext: IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Offers> Offers { get; set; }
+        public DbSet<UsersDto> UsersDto { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Offers>().ToTable("Offers");
+            modelBuilder.Entity<UsersDto>().ToTable("UsersDto");
+
+            modelBuilder.Entity<Offers>()
+                    .HasOne(o => o.Users)
+                    .WithMany(u => u.Offers)
+                    .HasForeignKey(o => o.UserId)
+                    .OnDelete(DeleteBehavior.NoAction);
+
+
+        }
+
+    }
+}
---
+++ Odtahovky.Server/Models/UsersDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Odtahovky.Server.Models
+{
+    public class UsersDto
+    {
+        [Key]
+        public Guid UserId { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+
+        //cizí klíč
+        public ICollection<Offers> Offers { get; set; } = new List<Offers>();
+
+    }
+}
